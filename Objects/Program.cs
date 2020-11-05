9011c21ba300084161b29fd024314441319cebb8
Lutz <lambda2@gmx.at>
2020-11-05 12:17:50 +0100

    added Properties and inheritance

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -8,11 +8,17 @@ namespace Objects
         {
             Auto CarA = new Auto();
             CarA.ReifenAnzahl = 5;
-            
+
 
             Ferrari FerrA = new Ferrari();
             FerrA.ReifenAnzahl = 4;
-            
+
+            CarA.GibMirMalVentile();
+            CarA.SetzeMalDieVentile(5);
+
+            CarA.Zylinder = 3;
+            Console.WriteLine(CarA.Zylinder);
+
         }
     }
 
@@ -25,11 +31,54 @@ namespace Objects
         protected int SitzAnzahl; // "Fammiliengeheimnis" Vererbt an Child-Objekte, aber nicht von anderen klassen sichtbar
         private int Ventile; // "geheim" nur von der gleichen klasse sichtbar, keine vererbung.
 
+        private int SchreibCounter = 0;
         void ZugriffstestB()
         {
             ReifenAnzahl = 2;
             SitzAnzahl = 2;
-            Ventile = 5; 
+            Ventile = 5;
+            _zylinder = 7;
+
+        }
+
+        private int _hitpoints;
+
+        public int Hitpoints
+        {
+            get { return _hitpoints; }
+            set
+            {
+                _hitpoints = value;
+                if (_hitpoints < 20)
+                {
+                    Zylinder--;
+                }
+            }
+        }
+
+
+        private int _zylinder;
+
+        public int Zylinder
+        {
+            get { return _zylinder; }
+            private set
+            { // alle im netzwerk aktualisieren
+                _zylinder = value;
+            }
+        }
+
+
+
+        public int GibMirMalVentile()
+        {
+            return Ventile;
+        }
+
+        public void SetzeMalDieVentile(int NeueVentilanzahl)
+        {
+            SchreibCounter++;
+            Ventile = NeueVentilanzahl;
         }
     }
 
