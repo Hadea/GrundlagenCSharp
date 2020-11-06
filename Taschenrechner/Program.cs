837a437d9ac62940d65bd7c05b90bffcbc63d158
Lutz <lambda2@gmx.at>
2020-11-06 15:34:29 +0100

    added multiple calculation support

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -17,62 +17,69 @@ namespace Taschenrechner
 
             do
             {
-                Console.Write("Please enter your first number : ");
-                UserInput = Console.ReadLine();
-            } while (!int.TryParse(UserInput, out ConvertedNumber));
-
-            Calculator calc = new Calculator();
-            calc.NumberA = ConvertedNumber;
-            bool faultyInput = true;
-
-            do
-            {
-                Console.Write("Please enter an operation ( + - * / % ) : ");
-                UserInput = Console.ReadLine();
-                switch (UserInput)
+                do
                 {
-                    case "+":
-                        calc.Operator = Operations.Addition;
-                        faultyInput = false;
-                        break;
-                    case "-":
-                        calc.Operator = Operations.Substration;
-                        faultyInput = false;
-                        break;
-                    case "*":
-                        calc.Operator = Operations.Multiplication;
-                        faultyInput = false;
-                        break;
-                    case "/":
-                        calc.Operator = Operations.Division;
-                        faultyInput = false;
-                        break;
-                    case "%":
-                        calc.Operator = Operations.Modulo;
-                        faultyInput = false;
-                        break;
-                    default:
-                        Console.WriteLine("Unkown operator, try again: ");
-                        break;
+                    Console.Write("Please enter your first number : ");
+                    UserInput = Console.ReadLine();
+                } while (!int.TryParse(UserInput, out ConvertedNumber));
+
+                Calculator calc = new Calculator();
+                calc.NumberA = ConvertedNumber;
+                bool faultyInput = true;
+
+                do
+                {
+                    Console.Write("Please enter an operation ( + - * / % ) : ");
+                    UserInput = Console.ReadLine();
+                    switch (UserInput)
+                    {
+                        case "+":
+                            calc.Operator = Operations.Addition;
+                            faultyInput = false;
+                            break;
+                        case "-":
+                            calc.Operator = Operations.Substration;
+                            faultyInput = false;
+                            break;
+                        case "*":
+                            calc.Operator = Operations.Multiplication;
+                            faultyInput = false;
+                            break;
+                        case "/":
+                            calc.Operator = Operations.Division;
+                            faultyInput = false;
+                            break;
+                        case "%":
+                            calc.Operator = Operations.Modulo;
+                            faultyInput = false;
+                            break;
+                        default:
+                            Console.WriteLine("Unkown operator, try again: ");
+                            break;
+                    }
+                } while (faultyInput);
+
+                do
+                {
+                    Console.Write("Please insert second number : ");
+                    UserInput = Console.ReadLine();
                 }
-            } while (faultyInput);
+                while (!int.TryParse(UserInput, out ConvertedNumber));
 
-            do
-            {
-                Console.Write("Please insert second number : ");
-                UserInput = Console.ReadLine();
-            }
-            while (!int.TryParse(UserInput, out ConvertedNumber));
+                calc.NumberB = ConvertedNumber;
 
-            calc.NumberB = ConvertedNumber;
+                if ((calc.Operator == Operations.Modulo || calc.Operator == Operations.Division) && calc.NumberB == 0)
+                {
+                    Console.WriteLine("Division by zero is undefined!");
+                    return;
+                }
 
-            if ((calc.Operator == Operations.Modulo || calc.Operator == Operations.Division) && calc.NumberB == 0)
-            {
-                Console.WriteLine("Division by zero is undefined!");
-                return;
-            }
+                Console.WriteLine("Result is: " + calc.Calculate());
+                Console.Write("Again?[y/n] : ");
+            } while (Console.ReadLine().ToLower() == "y" ); // nur wenn ein kleines y eingegeben wird soll wiederholt werden
 
-            Console.WriteLine("Result is: " + calc.Calculate());
+            Console.WriteLine("Ending program. Bye :)");
+
         }
     }
 }
