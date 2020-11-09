85584c7594f3ab5867dbf4e3589fd08640645d0b
Lutz <lambda2@gmx.at>
2020-11-09 11:19:28 +0100

    Added "KaffeeAutomat" to solution

---
+++ Kaffeeautomat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kaffeeautomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo User, dies ist ein Kaffeeautomat");
+            Console.WriteLine("Möchten sie einen Kaffee?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "y":
+                    Console.WriteLine("Ihr Kaffee wird ausgegeben");
+                    break;
+                default:
+                    Console.WriteLine("Schade, dann kann ich ihnen nicht helfen");
+                    break;
+            }
+
+            Console.WriteLine("Kaffeeautomat wird beendet");
+        }
+    }
+}
