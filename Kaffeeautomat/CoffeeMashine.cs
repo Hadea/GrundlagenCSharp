5e7c4533ec352f6d58e06ea5107ccc1a887764cb
Lutz <lambda2@gmx.at>
2020-11-09 13:02:58 +0100

    added seperate logic class

---
+++ Kaffeeautomat/CoffeeMashine.cs
@@ -0,0 +1,72 @@
+﻿namespace Kaffeeautomat
+{
+    class CoffeeMashine
+    {
+        byte containerCoffee;
+        byte containerWater;
+        byte containerWasteCoffee;
+        byte containerWasteWater;
+        byte containerMilk;
+
+        public byte CCoffee
+        {
+            get { return containerCoffee; }
+            set { containerCoffee = value; }
+        }
+        public byte CWater
+        {
+            get { return containerWater; }
+            set { containerWater = value; }
+        }
+        public byte CWasteWater
+        {
+            get { return containerWasteWater; }
+            set { containerWasteWater = value; }
+        }
+        public byte CWasteCoffee
+        {
+            get { return containerWasteCoffee; }
+            set { containerWasteCoffee = value; }
+        }
+
+
+
+        public CoffeeMashine()
+        {
+            containerCoffee = 100;
+            containerWater = 100;
+            containerWasteWater = 0;
+            containerWasteCoffee = 0;
+            containerMilk = 100;
+        }
+
+        public virtual bool Dispense(Recipe ChosenProduct)
+        {
+            switch (ChosenProduct)
+            {
+                case Recipe.Coffee:
+                    if (containerCoffee > 3 && containerWater > 10 && containerWasteWater < 99 && containerWasteCoffee < 97)
+                    {
+                        System.Threading.Thread.Sleep(4000);
+                        containerCoffee -= 3;
+                        containerWater -= 10;
+                        containerWasteWater += 1;
+                        containerWasteCoffee += 3;
+                        return true;
+                    }
+                    break;
+                case Recipe.HotWater:
+                    break;
+                case Recipe.Capuchino:
+                    break;
+                case Recipe.CoffeeMilk:
+                    break;
+                case Recipe.HotMilk:
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+}
--- Kaffeeautomat/Program.cs
+++ Kaffeeautomat/Program.cs
@@ -1,25 +1,57 @@
 ﻿using System;
+using System.Threading;
 
 namespace Kaffeeautomat
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hallo User, dies ist ein Kaffeeautomat");
-            Console.WriteLine("Möchten sie einen Kaffee?");
-            string userInput = Console.ReadLine();
+            Console.WriteLine("Hallo User, dies ist ein Kaffeeautomat\nKaffeemaschine wird gestartet");
+            CoffeeMashine coffeeMashine = new CoffeeMashine();
 
-            switch (userInput)
+            bool shutdownMashine = false;
+            do
             {
-                case "y":
-                    Console.WriteLine("Ihr Kaffee wird ausgegeben");
-                    break;
-                default:
-                    Console.WriteLine("Schade, dann kann ich ihnen nicht helfen");
-                    break;
-            }
+                Thread.Sleep(2000);
+                Console.Clear();
+                Console.WriteLine(" K -> Kaffee ausgeben\n C -> Capuchino\n H -> Heisses Wasser\n M -> Heisse Milch\n ? -> Milchkaffee\n W -> Wartung\n  A -> Kaffeemaschine abschalten");
+                string userInput = Console.ReadLine().ToUpper();
 
+                switch (userInput)
+                {
+                    case "k":
+                    case "K":
+                        Console.WriteLine("Ihr Kaffee wird zubereitet");
+                        if (coffeeMashine.Dispense(Recipe.Coffee))
+                        {
+                            Console.WriteLine("Ihr Kaffee ist fertig");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ihr Kaffee konnte nicht zubereitet werden, checken sie die Kontainer");
+                            Console.ResetColor();
+                        }
+                        break;
+                    case "A":
+                        shutdownMashine = true;
+                        break;
+                    case "W":
+                        coffeeMashine.CCoffee = 100;
+                        coffeeMashine.CWater = 100;
+                        coffeeMashine.CWasteCoffee = 0;
+                        coffeeMashine.CWasteWater = 0;
+                        Console.WriteLine("Wartung durchgeführt, container sind wieder ok.");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Die Eingabe verstehe ich nicht.");
+                        Console.ResetColor();
+                        break;
+                }
+
+            } while (!shutdownMashine);
             Console.WriteLine("Kaffeeautomat wird beendet");
         }
     }
