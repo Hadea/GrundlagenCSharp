d9ddaeceb6d6535d9f7f70a857ef30db48fd7e02
Lutz <lambda2@gmx.at>
2020-11-11 15:32:32 +0100

    added fps project

---
+++ FPSCounter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FPSCounter
+{
+    class Program
+    {
+        class FPS
+        {
+            DateTime lastUpdate;
+            uint framesSinceLastUpdate;
+
+            public FPS()
+            {
+                lastUpdate = DateTime.Now;
+                framesSinceLastUpdate = 0;
+            }
+            public void Draw()
+            {
+                framesSinceLastUpdate++;
+                if ((DateTime.Now - lastUpdate).TotalMilliseconds >= 5000)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write("{0,8} fps", framesSinceLastUpdate/5);
+                    framesSinceLastUpdate = 0;
+                    lastUpdate = DateTime.Now;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FPS fpsCounter = new FPS();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            do
+            {
+                fpsCounter.Draw();
+            } while (!Console.KeyAvailable);
+        }
+    }
+}
