8ae18b19c46c05eb47f220e292ddccce670852ea
Lutz <lambda2@gmx.at>
2020-11-12 08:28:09 +0100

    seperated classes and deactivated cursor. switched to .NET 5

---
+++ FPSCounter/FPS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSCounter
+{
+    class FPS
+    {
+        DateTime lastUpdate;
+        uint framesSinceLastUpdate;
+
+        public FPS()
+        {
+            lastUpdate = DateTime.Now;
+            framesSinceLastUpdate = 0;
+        }
+        public void Draw()
+        {
+            framesSinceLastUpdate++;
+            if ((DateTime.Now - lastUpdate).TotalMilliseconds >= 5000)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("{0,8} fps", framesSinceLastUpdate / 5);
+                framesSinceLastUpdate = 0;
+                lastUpdate = DateTime.Now;
+            }
+        }
+    }
+}
--- FPSCounter/Program.cs
+++ FPSCounter/Program.cs
@@ -4,32 +4,11 @@ namespace FPSCounter
 {
     class Program
     {
-        class FPS
-        {
-            DateTime lastUpdate;
-            uint framesSinceLastUpdate;
 
-            public FPS()
-            {
-                lastUpdate = DateTime.Now;
-                framesSinceLastUpdate = 0;
-            }
-            public void Draw()
-            {
-                framesSinceLastUpdate++;
-                if ((DateTime.Now - lastUpdate).TotalMilliseconds >= 5000)
-                {
-                    Console.SetCursorPosition(0, 0);
-                    Console.Write("{0,8} fps", framesSinceLastUpdate/5);
-                    framesSinceLastUpdate = 0;
-                    lastUpdate = DateTime.Now;
-                }
-            }
-        }
-
-        static void Main(string[] args)
+        static void Main()
         {
             FPS fpsCounter = new FPS();
+            Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.Yellow;
             do
             {
