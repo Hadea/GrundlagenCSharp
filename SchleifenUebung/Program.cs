25d5d7d8cee0c7f86c10f9e058fab8a0b107c226
Lutz <lambda2@gmx.at>
2020-11-16 13:02:00 +0100

    added project for loop and array excamples

---
+++ SchleifenUebung/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SchleifenUebung
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Hello World!";
+            Console.WriteLine(text);
+            // zählschleife von 0 solange textende nicht erreicht
+                // ausgabe des buchstabens an der stelle des zählers.
+
+            // text buchstabe für buchstabe ausgeben, diesmal aber rückwärts
+
+            // text ausgeben, aber nur jeden zweiten buchstaben
+
+            // text ausgeben, aber zähler immer verdoppeln
+
+        }
+    }
+}
