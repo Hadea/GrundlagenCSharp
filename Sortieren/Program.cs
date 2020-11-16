31fc86103b86ba63994c34e83284d017d1c485cb
Lutz <lambda2@gmx.at>
2020-11-16 11:00:49 +0100

    implemented first version of selectionsort

--- Sortieren/Program.cs
+++ Sortieren/Program.cs
@@ -4,22 +4,47 @@ namespace Sortieren
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            int[] arrayData = new int[8];
+            arrayRandomFill(arrayData);
+            printArray(arrayData);
+            sort(arrayData);
+            printArray(arrayData);
         }
 
-        static void Sort(int[] ArrayToSort)
+        static void printArray(int[] ArrayToPrint)
         {
-            // zählen von 0 solange kleiner als Arraylänge
-                //zählen von äuserem zählerstand solange kleiner als Arraylänge
-                    //wenn zahl an position des äusseren zählers grösser als die des inneren zählers
-                        // zahl an innerer position sichern
-                        // zahl an äusserer an position der inneren speichern
-                        // gesicherte an äusserer position speichern
-                    //ende wenn
-                // ende zählen
-            // ende zählen
+            foreach (int item in ArrayToPrint)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void arrayRandomFill(int[] ArrayToFill)
+        {
+            Random rndGen = new Random(); // erstellen eines zufallsgenerators
+            for (int counter = 0; counter < ArrayToFill.Length; counter++) // das übergebene array von anfang bis ende durchlaufen
+            {
+                ArrayToFill[counter] = rndGen.Next(100);// den aktuellen eintrag auf eine frisch generierte zufallszahl setzen
+            }
+        }
+
+        static void sort(int[] ArrayToSort)
+        {
+            for (int outer = 0; outer < ArrayToSort.Length; outer++)// zählen von 0 solange kleiner als Arraylänge
+            {
+                for (int inner = outer; inner < ArrayToSort.Length; inner++)//zählen von äuserem zählerstand solange kleiner als Arraylänge
+                {
+                    if (ArrayToSort[outer] > ArrayToSort[inner])//wenn zahl an position des äusseren zählers grösser als die des inneren zählers
+                    {
+                        int backup = ArrayToSort[inner];// zahl an innerer position sichern
+                        ArrayToSort[inner] = ArrayToSort[outer];// zahl an äusserer an position der inneren speichern
+                        ArrayToSort[outer] = backup;// gesicherte an äusserer position speichern
+                    }//ende wenn
+                }// ende zählen
+            }// ende zählen 
         }
     }
 }
