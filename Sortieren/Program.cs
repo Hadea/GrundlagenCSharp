6f2b76f52beaa0662015a114bdb3b970240a3f0e
Lutz <lambda2@gmx.at>
2020-11-16 09:17:05 +0100

    added sorting project

---
+++ Sortieren/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sortieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Sort(int[] ArrayToSort)
+        {
+            // zählen von 0 solange kleiner als Arraylänge
+                //zählen von äuserem zählerstand solange kleiner als Arraylänge
+                    //wenn zahl an position des äusseren zählers grösser als die des inneren zählers
+                        // zahl an innerer position sichern
+                        // zahl an äusserer an position der inneren speichern
+                        // gesicherte an äusserer position speichern
+            // ende zählen
+        }
+    }
+}
