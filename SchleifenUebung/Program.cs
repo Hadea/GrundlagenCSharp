46e91def3d81c3f7bb44b5cc6db06b38a362aeb2
Lutz <lambda2@gmx.at>
2020-11-16 15:33:51 +0100

    removed possibility of duplicate numbers

--- SchleifenUebung/Program.cs
+++ SchleifenUebung/Program.cs
@@ -1,10 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchleifenUebung
 {
     class Program
     {
         static void Main()
+        {
+            //TextSchleifen();
+            for (int i = 0; i < 20; i++)
+            {
+
+            EuroJackpot();
+            }
+
+
+
+        }
+
+        static void EuroJackpot()
+        {
+            Random rndGen = new Random();
+
+            List<int> numbers = new List<int>();
+            while (numbers.Count < 5)
+            {
+                int newNumber = rndGen.Next(1, 51);
+                bool insertAllowed = true;
+
+                foreach (var item in numbers)
+                {
+                    if (item == newNumber)
+                    {
+                        insertAllowed = false;
+                        break;
+                    }
+                }
+
+                if (insertAllowed)
+                {
+                    numbers.Add(newNumber);
+                }
+
+            }
+
+            List<int> zusatz = new List<int>();
+            while (zusatz.Count < 2)
+            {
+                int newNumber = rndGen.Next(1, 11);
+                bool insertAllowed = true;
+
+                foreach (var item in zusatz)
+                {
+                    if (item == newNumber)
+                    {
+                        insertAllowed = false;
+                        break;
+                    }
+                }
+
+                if (insertAllowed)
+                {
+                    zusatz.Add(newNumber);
+                }
+
+            }
+
+            Console.Write("EuroJackpot Zahlen:");
+            foreach (var item in numbers)
+            {
+                Console.Write("{0,3}",item);
+            }
+            Console.Write(" Zusatz:");
+            foreach (var item in zusatz)
+            {
+                Console.Write("{0,3}",item);
+            }
+            Console.WriteLine();
+
+        }
+
+        static void TextSchleifen()
         {
             string text = "Hello World!";
             Console.WriteLine(text);
@@ -30,7 +106,19 @@ namespace SchleifenUebung
             {
                 Console.Write(text[counter]);
             }
-            
+            Console.WriteLine();
+            Random rndGen = new Random();
+            for (int counter = 0; counter < text.Length; counter += rndGen.Next(3))
+            {
+                Console.Write(text[counter]);
+            }
+            Console.WriteLine();
+
+            for (int counter = 0; counter < text.Length; counter++)
+            {
+                Console.Write(text[rndGen.Next(text.Length)]);
+            }
+            // ausgabe von zufälligen zeichen aus dem text, genausooft wie der text zeichen hat
 
 
             Console.ReadLine();
