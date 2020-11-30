6b7507d619abed871645b257ddbec872a651c825
Lutz <lambda2@gmx.at>
2020-11-30 13:01:18 +0100

    Added Speech

---
+++ Speech/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Speech
+{
+    class Program
+    {
+        static void Main()
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SelectVoice("Microsoft Hedda Desktop"); // namen der stimmen können mit GetInstalledVoices() herausgefunden werden. englisch geht immer
+            synth.Speak("Hallo Welt");
+            Console.ReadLine();
+        }
+    }
+}
